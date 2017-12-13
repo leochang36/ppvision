38f099232864f21c48266fbbc34e27a829b7ffa3
leochang <31495587+leochang36@users.noreply.github.com>
2017-12-13 14:52:56 +0800

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,15 @@
+list1=[1,4,5,10,100,65535,-10,-99]
+print(max(list1))
+print(min(list1))
+list1.append('Hello')
+print(list1)
+list1.append(10)
+print(list1.count(10))
+print(list1.index(10))
+list1.remove('Hello')
+print(list1.sort())
+print(list1)
+
+
+
+
